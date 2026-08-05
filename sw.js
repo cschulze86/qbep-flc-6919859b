419434f6c2390b1be2a2a8dd19430ba40ab12309
Charles Schulze <c.schulze@thuso.co.uk>
2026-08-05 23:30:14 +0300

    Wellbeing items follow navigation language (EN/MY only)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var C="qbep-formative-0686e941eb";
+var C="qbep-formative-802f71ef1d";
 self.addEventListener("install",function(e){self.skipWaiting();e.waitUntil(caches.open(C).then(function(c){return c.addAll(["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png"])}))});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}))});
 self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(function(r){return r||fetch(e.request)}))});
